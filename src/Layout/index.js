ccfcb48e97396d09bc5a0c01ce6d15d1c5498f48
Carlo Santos <rcarlosantos89@gmail.com>
2024-04-29 13:23:14 -0500

    next is to map through the decks

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
+import { listDecks } from "../utils/api";
 
 function Layout() {
-  return (
-    <>
-      <Header />
-      <div className="container">
-        {/* TODO: Implement the screen starting here */}
-        <NotFound />
-      </div>
-    </>
-  );
+
+  // console.log(listDecks());
+  const [decks, setDecks] = useState([]);
+
+  useEffect(() => {
+    async function loadDeck() {
+      const response = await listDecks();
+      setDecks(response);
+    }
+    loadDeck();
+
+  }, [])
+
+  if (decks.length > 0) {
+    return (
+      <>
+        <Header />
+        <div className="container">
+          {/* TODO: Implement the screen starting here */}
+          <button>Create Deck</button>
+
+          <section className="card">
+            <h4>{decks[0].name}</h4>
+            <p>lorem ispusmlkjdslkajdfl;kja;lsdkfj;lajdl;f</p>
+            <button className="bg-secondary">View</button>
+            <button className="bg-primary">Study</button>
+            <button className="bg-danger">Delete</button>
+          </section>
+          <NotFound />
+        </div>
+      </>
+    );
+  } else {
+    return null;
+  }
+
 }
 
 export default Layout;
